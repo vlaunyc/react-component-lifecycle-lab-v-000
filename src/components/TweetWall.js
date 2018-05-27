fb255b4f3cf731b1f982e0f0e5f9362b514410b2
Vicky Lau <vlau.nyc@gmail.com>
2018-05-27 20:16:26 +0000

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,19 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount(){
+    this.setState({tweets: this.props.newTweets});
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: [...nextProps.newTweets, ...this.state.tweets]
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    return nextProps.newTweets.length > 0;
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
